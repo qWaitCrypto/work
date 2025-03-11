81cfce66afc336e1e949e5468ab6c80e838bb7ce
qWaitCrypto <axmaiqiu@gmail.com>
2025-03-11 11:27:46 +0800

    下载

--- download.py
+++ download.py
@@ -1,12 +1,17 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoProcessor, AutoModelForImageTextToText
 import os
 
 # 设置 HTTP 和 HTTPS 代理
 os.environ['HTTP_PROXY'] = 'http://127.0.0.1:7890'
 os.environ['HTTPS_PROXY'] = 'http://127.0.0.1:7890'
-# 下载并加载 Llama2 模型和分词器
-model_name = "meta-llama/Llama-2-13b-chat-hf"
-model = AutoModelForCausalLM.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model.save_pretrained('/model')
-tokenizer.save_pretrained('/model')
+
+# import torch
+# from modelscope import snapshot_download, AutoModel, AutoTokenizer
+# from modelscope import GenerationConfig
+# model_dir = snapshot_download('qwen/Qwen-7B-Chat', cache_dir='/root/autodl-tmp', revision='v1.1.4')
+#
+# exit()
+
+processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct")
+model = AutoModelForImageTextToText.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct")
+
